220c655370a18ab85417a8bff10a8e425ce2b3a0
David <oliveiradavid.dev@gmail.com>
2024-05-20 23:36:58 -0400

    backend: add database config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,39 @@
 import { Module } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    UsersModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: config.get('DB_TYPE'),
+          host: config.get('DB_HOST'),
+          port: config.get('DB_PORT'),
+          username: config.get('DB_USER'),
+          password: config.get('DB_PASSWORD'),
+          database: config.get('DB_NAME'),
+          entities: [__dirname + '/../**/*.entity.js'],
+          synchronize: config.get('NODE_ENV') === 'development',
+        } as TypeOrmModuleOptions;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
